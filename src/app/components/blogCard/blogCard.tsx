f805eddac764601c222646471d48eeaf6a9c6090
fikrinadzif <fikrinadzif35@gmail.com>
2024-07-06 09:21:05 +0700

    blog card component

--- src/app/components/blogCard/blogCard.tsx
+++ src/app/components/blogCard/blogCard.tsx
@@ -6,19 +6,19 @@ export default function BlogCard() {
   return (
     <div className="card group p-6 rounded-lg relative bg-[url('/images/photo.jpg')] h-[440px] bg-origin-border hover:bg-origi bg-center">
       <div className="absolute bottom-[18px] w-full left-0 right-0 p-3">
-        <h3 className="mb-[14px] font-extrabold text-3xl text-slate-50 drop-shadow-md">
+        <h3 className="mb-[12px] font-extrabold text-3xl text-slate-50 drop-shadow-md">
           Perfect for lightweight applications where bulk is undesirable
         </h3>
         <div className="flex group-hover:align-middle justify-between w-full">
-          <h6 className="font-bold text-slate-50 text-sm drop-shadow-md">
+          <h6 className="font-bold py-3 text-slate-50 text-sm drop-shadow-md">
             JAN 15, 2023
           </h6>
           <Link
             href="/"
-            className="bg-lime-500 align-middle w-[142px] font-bold text-slate-50 text-sm drop-shadow-md hidden group-hover:flex group-hover:animate-slideIn hover:cursor-pointer fill-slate-50 justify-between"
+            className="items-center w-[142px] font-bold text-slate-50 text-sm drop-shadow-md hidden group-hover:flex group-hover:animate-slideIn hover:cursor-pointer justify-between"
           >
             READ MORE
-            <ArrowRightIcon className="fill-slate-50" />
+            <ArrowRightIcon className="text-slate-50 text-4xl stroke-2" />
           </Link>
         </div>
       </div>
