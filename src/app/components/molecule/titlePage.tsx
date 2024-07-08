94e1907874aebb9be27e30cf84672d4d90ec6889
fikrinadzif <fikrinadzif35@gmail.com>
2024-07-08 19:58:47 +0700

    title page size, add image

--- src/app/components/molecule/titlePage.tsx
+++ src/app/components/molecule/titlePage.tsx
@@ -6,7 +6,7 @@ interface ITitlePage {
 
 export default function TitlePage(props: ITitlePage) {
   return (
-    <h1 className="text-4xl md:text-8xl font-extrabold text-center py-4 md:py-14">
+    <h1 className="text-2xl md:text-5xl font-extrabold text-center py-4 md:py-14">
       {props.title}
     </h1>
   );
